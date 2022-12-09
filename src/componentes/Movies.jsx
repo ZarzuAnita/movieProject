c41d783c3889b5f5e786a237fef5f09fb5e06e48
Ana Zarzuela <zarzuela.fernandez.ana@gmail.com>
2022-12-09 18:06:49 +0100

    llamada endpoint discover y primera entrada de gender action

--- src/componentes/Movies.jsx
+++ src/componentes/Movies.jsx
@@ -1,24 +1,53 @@
 import axios from 'axios'
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
+import { AiFillPlayCircle } from 'react-icons/ai'
+import { useState, useEffect } from 'react';
+import notAvailable from './notAvailable.png';
+import _ from 'lodash';
+import '../styles/moviesStyle.css'
 
- function Movies() {
-  const Api = 'https://api.themoviedb.org/3/discover/movie'
-  const MovieCall = async () =>{
-      const data = await axios.get (Api,{
-        params: {
-          api_key:'3bb50757489312cdb75262aa3722fec4'
-        }
-        })
-        console.log (data.data.results)
+function Movies() {
+
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  const Api = 'https://api.themoviedb.org/3/discover/movie';
+  const Images = 'https://image.tmdb.org/t/p/w500';
+
+  const PopularMoviesCall = async () => {
+    const respMovie = await axios.get(Api, {
+      params: {
+        api_key: '3bb50757489312cdb75262aa3722fec4',
+        language: 'en-US',
+        include_adult:'default',
+        include_video:'default',
+        with_genres:'28'
+
       }
-        useEffect(() =>{
-    MovieCall()
-  }, [])      
-    return (
+    })
+    const results = respMovie.data.results;
+    setPopularMovies(results)
+    console.log(popularMovies)
+
+  }
+  useEffect(() => {
+    PopularMoviesCall()
+  }, [])
+  console.log(popularMovies)
+
+
+  return (
     <Fragment>
-      <h1>Movies</h1>
+      <div className="tablero">
+          <h2>Action</h2> 
+              {popularMovies.map((movie) => (
+              <div className='movies'>                
+                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
+                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
+              </div>
+            ))}
+      </div>
+    
     </Fragment>
-  
   )
 };
 
