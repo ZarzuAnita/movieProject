01f220c14df6514684c281a5429497f0d7054048
Ana Zarzuela <zarzuela.fernandez.ana@gmail.com>
2022-12-18 16:41:57 +0100

    carousel añadido a la página de películas

--- src/componentes/Movies.jsx
+++ src/componentes/Movies.jsx
@@ -1,9 +1,19 @@
 import axios from 'axios'
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import { AiFillPlayCircle } from 'react-icons/ai'
 import { useState, useEffect } from 'react';
 import notAvailable from './notAvailable.png';
+import Slider from 'react-slick';
+import "./SimpleSlider/slick.css"; 
+import "./SimpleSlider/slick-theme.css";
 import '../styles/moviesStyle.css'
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles'
 
 
 function Movies() {
@@ -14,6 +24,7 @@ function Movies() {
   const [sf, setSFMovies] = useState([])
   const [western, setWesternMovies] = useState([])
 
+  
 
   const Api = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`;
   const Images = 'https://image.tmdb.org/t/p/w500';
@@ -95,68 +106,121 @@ function Movies() {
     console.log(western)
 
 
+
   }
-  useEffect(() => {
+
+useEffect(() => {
     PopularMoviesCall()
   }, [])
   console.log(popularMovies)
 
 
+    const settings = {
+      className: "center",
+      centerMode: true,
+      centerPadding: "60px",
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll:4,
+      speed: 500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
   return (
     <Fragment>
-      <div className="tablero">
-          <h2>Action</h2> 
+   
+      <div className="simple">
+          <h2>Action</h2>
+            <Slider {...settings}>
               {popularMovies.map((movie) => (
-              <div className='movies'>                
+              <div>                         
+                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
+                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
+              </div>
+                   
+            ))}
+           
+            </Slider> 
+            
+      </div>
+      <div className="simple">
+          <h2>Comedy</h2>
+          <Slider {...settings}> 
+              {comedy.map((movie) => (
+              <div className=''>                
                 <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
                 <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
               </div>
             ))}
+        </Slider>
+      </div>
+
+      <div className="simple">
+          <h2>Drama</h2> 
+          <Slider {...settings}>
+              {drama.map((movie) => (
+              <div className=''>                
+                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
+                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
+              </div>
+            ))}
+          </Slider>
+      </div>
+
+
+      <div className="simple">
+          <h2>Science Fiction</h2> 
+          <Slider {...settings}>
+              {sf.map((movie) => (
+              <div className=''>                
+                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
+                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
+              </div>
+            ))}
+          </Slider>
+      </div>
+
+      <div className="simple">
+          <h2>Western</h2>
+          <Slider {...settings}> 
+              {western.map((movie) => (
+              <div className=''>                
+                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
+                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
+              </div>
+            ))}
+         </Slider>
       </div>
     
-      <div className="tablero">
-          <h2>Comedy</h2> 
-              {comedy.map((movie) => (
-              <div className='movies'>                
-                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
-                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
-              </div>
-            ))}
-      </div>
-
-      <div className="tablero">
-          <h2>Drama</h2> 
-              {drama.map((movie) => (
-              <div className='movies'>                
-                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
-                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
-              </div>
-            ))}
-      </div>
-
-
-      <div className="tablero">
-          <h2>Science Fiction</h2> 
-              {sf.map((movie) => (
-              <div className='movies'>                
-                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
-                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
-              </div>
-            ))}
-      </div>
-
-      <div className="tablero">
-          <h2>Western</h2> 
-              {western.map((movie) => (
-              <div className='movies'>                
-                <img width='200' src={movie.poster_path ? `${Images}${movie.poster_path}` : notAvailable} alt='' />
-                <AiFillPlayCircle color='purple' fontSize={45} id='playIcon' />
-              </div>
-            ))}
-      </div>
     </Fragment>
   )
-};
+              }             
+
 
 
 export default Movies;
